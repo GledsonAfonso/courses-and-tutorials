fc7f9c1d561fccd502c3560b1f9aed2de2c7f673
Gledson A <g_a6@live.com>
2023-03-13 02:26:35 -0300

    [Book - Desb. SOLID] - Applying the DIP for the ebook generators again (decreasing the number of calls using a factory this time)

--- books/desbravando-solid/src/main/java/cotuba/application/EbookGenerator.java
+++ books/desbravando-solid/src/main/java/cotuba/application/EbookGenerator.java
@@ -1,7 +1,19 @@
 package cotuba.application;
 
 import cotuba.domain.Ebook;
+import cotuba.epub.EpubGenerator;
+import cotuba.pdf.PdfGenerator;
 
 public interface EbookGenerator {
   void generate(Ebook ebook);
+  
+  static EbookGenerator create(String format) {
+    if ("pdf".equalsIgnoreCase(format)) {
+      return new PdfGenerator();
+    } else if ("epub".equalsIgnoreCase(format)) {
+      return new EpubGenerator();
+    } else {
+      throw new IllegalArgumentException("Invalid ebook format: " + format);
+    }
+  }
 }
--- books/desbravando-solid/src/main/java/cotuba/application/Orchestrator.java
+++ books/desbravando-solid/src/main/java/cotuba/application/Orchestrator.java
@@ -1,9 +1,7 @@
 package cotuba.application;
 
 import cotuba.domain.Ebook;
-import cotuba.epub.EpubGenerator;
 import cotuba.md.HtmlRender;
-import cotuba.pdf.PdfGenerator;
 
 public class Orchestrator {
   public void execute(OrchestratorParameters parameters) {
@@ -15,14 +13,7 @@ public class Orchestrator {
     ebook.setOutputPath(parameters.getOutputPath());
     ebook.setChapters(chapters);
 
-    if ("pdf".equals(parameters.getFormat())) {
-      var pdfGenerator = new PdfGenerator();
-      pdfGenerator.generate(ebook);
-    } else if ("epub".equals(parameters.getFormat())) {
-      var epubGenerator = new EpubGenerator();
-      epubGenerator.generate(ebook);
-    } else {
-      throw new IllegalArgumentException("Invalid ebook format: " + parameters.getFormat());
-    }
+    var ebookGenerator = EbookGenerator.create(parameters.getFormat());
+    ebookGenerator.generate(ebook);
   }
 }
