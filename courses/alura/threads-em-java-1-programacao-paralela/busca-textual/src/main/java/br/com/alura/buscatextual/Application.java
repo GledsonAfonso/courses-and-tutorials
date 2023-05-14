eeb710b12fb0bb820128294e88342a901d1b457d
Gledson A <g_a6@live.com>
2023-05-14 00:10:05 -0300

    Course - Alura - Threads em Java 1: Programação Paralela - Chap. 1

---
+++ courses/alura/threads-em-java-1-programacao-paralela/busca-textual/src/main/java/br/com/alura/buscatextual/Application.java
@@ -0,0 +1,14 @@
+package br.com.alura.buscatextual;
+
+public class Application {
+	public static void main(String[] args) {
+		var name = "do";
+		var firstThread = new Thread(new TextualSearchText("assinaturas1.txt", name));
+		var secondThread = new Thread(new TextualSearchText("assinaturas2.txt", name));
+		var thirdThread = new Thread(new TextualSearchText("autores.txt", name));
+
+		firstThread.start();
+		secondThread.start();
+		thirdThread.start();
+	}
+}
---
+++ courses/alura/threads-em-java-1-programacao-paralela/busca-textual/src/main/java/br/com/alura/buscatextual/TextualSearchText.java
@@ -0,0 +1,32 @@
+package br.com.alura.buscatextual;
+
+import java.io.File;
+import java.util.Scanner;
+
+public class TextualSearchText implements Runnable {
+  private final String filepath;
+  private final String name;
+
+  public TextualSearchText(String filepath, String name) {
+    this.filepath = filepath;
+    this.name = name;
+  }
+
+  @Override
+  public void run() {
+    try (var scanner = new Scanner(new File(this.filepath))) {
+      long lineNumber = 1;
+      while (scanner.hasNextLine()) {
+        var line = scanner.nextLine();
+
+        if (line.toLowerCase().contains(this.name.toLowerCase())) {
+          System.out.println(Thread.currentThread().threadId() + " - " + this.filepath + " - " + lineNumber + " - " + line);
+        }
+
+        lineNumber++;
+      }
+    } catch (Exception exception) {
+      throw new RuntimeException(exception);
+    }
+  }
+}
